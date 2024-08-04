ce2d8308f77795abe22a8174988bd9cc17968cef
prithvi <prith15viraj@gmail.com>
2024-08-04 11:25:29 +0530

    do changes ajax call

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,122 @@
+// $(document).ready(function () {
+//     const data = localStorage.getItem("EmployeeData");
+//     const empData = JSON.parse(data);
+//     displayTable(empData);
+
+//     function displayTable(empData) {
+//         const addbtn = $('#addbtn');
+//         const empTable = $('#table-ctn');
+//         const searchbtn = $('#searchbtn');
+//         const searchTxt = $('#searchtxt');
+//         let template = `<table>
+//             <tr>
+//                 <th>Name</th>
+//                 <th>Gender</th>
+//                 <th>Department</th>
+//                 <th>Salary</th>
+//                 <th>Start Date</th>
+//                 <th>Actions</th>
+//             </tr>`;
+
+//         if (empData && empData.length > 0) {
+//             searchbtn.on("click", function () {
+//                 searchAction(empData, empTable);
+//             });
+//             empData.forEach(ele => {
+//                 let depTemp = "";
+//                 ele.Dept.forEach(dept => {
+//                     depTemp += `<div class="dept-ctn">${dept}</div>`;
+//                 });
+//                 template += `<tr>
+//                     <td><div class="profileImg"><img src="/assets/${ele.empImg}.png" alt="profileimg"><label>${ele.Name}</label></div></td>
+//                     <td>${ele.Gender}</td>
+//                     <td><div class="dept-outter">${depTemp}</div></td>
+//                     <td>₹ ${ele.Salary} LPA</td>
+//                     <td>${ele.StartDate}</td>
+//                     <td><div class="action-btn"><img src="/assets/bin.png" alt="delete" width="20px" height="24px" onclick="deleteEmp('${ele.Name}')"> <img src="/assets/edit.png" alt="edit" width="20px" height="20px" onclick="editEmp('${ele.Name}')"></div></td>
+//                 </tr>`;
+//             });
+//             template += `</table>`;
+//             empTable.html(template);
+//         } else {
+//             empTable.html("<center><h1>No Data Found!</h1></center>");
+//         }
+
+//         addbtn.on("click", function () {
+//             window.location.href = "/index.html";
+//         });
+
+//         searchTxt.on("search", function () {
+//             location.reload();
+//         });
+//     }
+
+//     window.deleteEmp = function (empName) {
+//         const data = localStorage.getItem("EmployeeData");
+//         const empData = JSON.parse(data);
+//         const updatedData = empData.filter(ele => ele.Name !== empName);
+//         if (updatedData.length) {
+//             localStorage.setItem("EmployeeData", JSON.stringify(updatedData));
+//         } else {
+//             localStorage.removeItem("EmployeeData");
+//         }
+//         location.reload();
+//     };
+
+//     window.editEmp = function (empName) {
+//         const data = localStorage.getItem("EmployeeData");
+//         const empData = JSON.parse(data);
+//         const editEmp = empData.filter(ele => ele.Name === empName);
+//         localStorage.setItem("EditEmpData", JSON.stringify(editEmp));
+//         window.location.href = "/index.html";
+//     };
+
+//     function searchAction(empData, empTable) {
+//         const searchTxt = $('#searchtxt');
+//         let template = `<table>
+//             <tr>
+//                 <th>Name</th>
+//                 <th>Gender</th>
+//                 <th>Department</th>
+//                 <th>Salary</th>
+//                 <th>Start Date</th>
+//                 <th>Actions</th>
+//             </tr>`;
+//         const ele = empData.filter(e => e.Name === searchTxt.val());
+//         if (ele.length > 0) {
+//             ele.forEach(e => {
+//                 let depTemp = "";
+//                 e.Dept.forEach(dept => {
+//                     depTemp += `<div class="dept-ctn">${dept}</div>`;
+//                 });
+//                 template += `<tr>
+//                     <td><div class="profileImg"><img src="/assets/${e.empImg}.png" alt="profileimg"><label>${e.Name}</label></div></td>
+//                     <td>${e.Gender}</td>
+//                     <td><div class="dept-outter">${depTemp}</div></td>
+//                     <td>₹ ${e.Salary} LPA</td>
+//                     <td>${e.StartDate}</td>
+//                     <td><div class="action-btn"><img src="/assets/bin.png" alt="delete" width="20px" height="24px" onclick="deleteEmp('${e.Name}')"> <img src="/assets/edit.png" alt="edit" width="20px" height="20px" onclick="editEmp('${e.Name}')"></div></td>
+//                 </tr>`;
+//             });
+//             template += `</table>`;
+//             empTable.html(template);
+//         } else if (!searchTxt.val()) {
+//             location.reload();
+//         } else {
+//             empTable.html("<center><h1>No Data Found!</h1></center>");
+//         }
+//     }
+// });
+
+
 $(document).ready(function () {
-    const data = localStorage.getItem("EmployeeData");
-    const empData = JSON.parse(data);
-    displayTable(empData);
+    $.ajax({
+        url: "http://localhost:3000/emp_data",
+        method: "GET",
+        success: function (data) {
+            displayTable(data);
+        }
+    });
 
     function displayTable(empData) {
         const addbtn = $('#addbtn');
@@ -33,7 +148,7 @@ $(document).ready(function () {
                     <td><div class="dept-outter">${depTemp}</div></td>
                     <td>₹ ${ele.Salary} LPA</td>
                     <td>${ele.StartDate}</td>
-                    <td><div class="action-btn"><img src="/assets/bin.png" alt="delete" width="20px" height="24px" onclick="deleteEmp('${ele.Name}')"> <img src="/assets/edit.png" alt="edit" width="20px" height="20px" onclick="editEmp('${ele.Name}')"></div></td>
+                    <td><div class="action-btn"><img src="/assets/bin.png" alt="delete" width="20px" height="24px" onclick="deleteEmp('${ele.id}')"> <img src="/assets/edit.png" alt="edit" width="20px" height="20px" onclick="editEmp('${ele.id}')"></div></td>
                 </tr>`;
             });
             template += `</table>`;
@@ -51,24 +166,25 @@ $(document).ready(function () {
         });
     }
 
-    window.deleteEmp = function (empName) {
-        const data = localStorage.getItem("EmployeeData");
-        const empData = JSON.parse(data);
-        const updatedData = empData.filter(ele => ele.Name !== empName);
-        if (updatedData.length) {
-            localStorage.setItem("EmployeeData", JSON.stringify(updatedData));
-        } else {
-            localStorage.removeItem("EmployeeData");
-        }
-        location.reload();
+    window.deleteEmp = function (id) {
+        $.ajax({
+            url: `http://localhost:3000/emp_data/${id}`,
+            method: "DELETE",
+            success: function () {
+                location.reload();
+            }
+        });
     };
 
-    window.editEmp = function (empName) {
-        const data = localStorage.getItem("EmployeeData");
-        const empData = JSON.parse(data);
-        const editEmp = empData.filter(ele => ele.Name === empName);
-        localStorage.setItem("EditEmpData", JSON.stringify(editEmp));
-        window.location.href = "/index.html";
+    window.editEmp = function (id) {
+        $.ajax({
+            url: `http://localhost:3000/emp_data/${id}`,
+            method: "GET",
+            success: function (data) {
+                localStorage.setItem("EditEmpData", JSON.stringify([data]));
+                window.location.href = "/index.html";
+            }
+        });
     };
 
     function searchAction(empData, empTable) {
@@ -95,7 +211,7 @@ $(document).ready(function () {
                     <td><div class="dept-outter">${depTemp}</div></td>
                     <td>₹ ${e.Salary} LPA</td>
                     <td>${e.StartDate}</td>
-                    <td><div class="action-btn"><img src="/assets/bin.png" alt="delete" width="20px" height="24px" onclick="deleteEmp('${e.Name}')"> <img src="/assets/edit.png" alt="edit" width="20px" height="20px" onclick="editEmp('${e.Name}')"></div></td>
+                    <td><div class="action-btn"><img src="/assets/bin.png" alt="delete" width="20px" height="24px" onclick="deleteEmp('${e.id}')"> <img src="/assets/edit.png" alt="edit" width="20px" height="20px" onclick="editEmp('${e.id}')"></div></td>
                 </tr>`;
             });
             template += `</table>`;
--- index.js
+++ index.js
@@ -1,3 +1,90 @@
+// $(document).ready(function () {
+//     function getIds() {
+//         return {
+//             empName: $('#formname'),
+//             profileImg: $('.profile'),
+//             empGender: $('.gender'),
+//             salaryrange: $('#salary'),
+//             salarytxt: $('#salarytxt'),
+//             startday: $('#startday'),
+//             startmonth: $('#startmonth'),
+//             startyear: $('#startyear'),
+//             notes: $('#notes'),
+//             dept: $('.dept')
+//         };
+//     }
+
+//     function resetForm() {
+//         const ids = getIds();
+//         ids.empName.val('');
+//         ids.profileImg.prop('checked', false);
+//         ids.empGender.prop('checked', false);
+//         ids.salaryrange.val(4);
+//         ids.salarytxt.val('');
+//         ids.startday.val('');
+//         ids.startmonth.val('');
+//         ids.startyear.val('');
+//         ids.notes.val('');
+//         ids.dept.prop('checked', false);
+//     }
+
+//     $('#salary').on('input', function () {
+//         $('#salarytxt').val($(this).val());
+//     });
+
+//     $('#submit-btn').on('click', function () {
+//         const ids = getIds();
+//         const employee = {
+//             Name: ids.empName.val(),
+//             empImg: ids.profileImg.filter(':checked').val(),
+//             Gender: ids.empGender.filter(':checked').val(),
+//             Dept: ids.dept.filter(':checked').map(function () { return this.value; }).get(),
+//             Salary: ids.salaryrange.val(),
+//             StartDate: `${ids.startday.val()} ${ids.startmonth.val()} ${ids.startyear.val()}`,
+//             Notes: ids.notes.val()
+//         };
+
+//         let storedData = [];
+//         if (localStorage.length) {
+//             storedData = JSON.parse(localStorage.getItem("EmployeeData"));
+//         }
+//         storedData.push(employee);
+//         localStorage.setItem("EmployeeData", JSON.stringify(storedData));
+
+//         window.location.href = "/dashboard.html";
+//     });
+
+//     $('#reset-btn').on('click', function () {
+//         resetForm();
+//     });
+
+//     $('#cancel-btn').on('click', function () {
+//         window.location.href = "/dashboard.html";
+//     });
+
+//     // Populate form if editing
+//     const editEmpData = localStorage.getItem("EditEmpData");
+//     if (editEmpData) {
+//         const employee = JSON.parse(editEmpData)[0];
+//         const ids = getIds();
+//         ids.empName.val(employee.Name);
+//         ids.profileImg.filter(`[value="${employee.empImg}"]`).prop('checked', true);
+//         ids.empGender.filter(`[value="${employee.Gender}"]`).prop('checked', true);
+//         ids.salaryrange.val(employee.Salary);
+//         ids.salarytxt.val(employee.Salary);
+//         const [day, month, year] = employee.StartDate.split(' ');
+//         ids.startday.val(day);
+//         ids.startmonth.val(month);
+//         ids.startyear.val(year);
+//         ids.notes.val(employee.Notes);
+//         ids.dept.each(function () {
+//             $(this).prop('checked', employee.Dept.includes(this.value));
+//         });
+//         localStorage.removeItem("EditEmpData");
+//     }
+// });
+
+
 $(document).ready(function () {
     function getIds() {
         return {
@@ -44,14 +131,15 @@ $(document).ready(function () {
             Notes: ids.notes.val()
         };
 
-        let storedData = [];
-        if (localStorage.length) {
-            storedData = JSON.parse(localStorage.getItem("EmployeeData"));
-        }
-        storedData.push(employee);
-        localStorage.setItem("EmployeeData", JSON.stringify(storedData));
-
-        window.location.href = "/dashboard.html";
+        $.ajax({
+            url: "http://localhost:3000/emp_data",
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(employee),
+            success: function () {
+                window.location.href = "/dashboard.html";
+            }
+        });
     });
 
     $('#reset-btn').on('click', function () {
